97d7d9df011805b8c5a10fc1e364db013f14b526
Kyrylo Sheludchenko <kirill.shelydchenko@gmail.com>
2025-05-05 10:10:55 +0200

    Project Layout

--- CSS Library/gulpfile.js
+++ CSS Library/gulpfile.js
@@ -1,14 +1,18 @@
+// gulpfile.js
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
+// compile only your main entry-point
 function buildStyles() {
   return src("./scss/style.scss")
-    .pipe(sass())
-    .pipe(dest("./css/"))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./css/"));
 }
 
-function watchTask() {
-  watch(["./scss/style.scss"], buildStyles)
+// watch but then call done()
+function watchTask(done) {
+  watch("./scss/**/*.scss", buildStyles);
+  done();
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
