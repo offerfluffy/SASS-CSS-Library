4d3d129dd17719dd9ec83f747d84e1e943fc9d5e
Kyrylo Sheludchenko <kirill.shelydchenko@gmail.com>
2025-05-06 12:13:03 +0200

    Purging CSS

--- CSS Library/gulpfile.js
+++ CSS Library/gulpfile.js
@@ -1,17 +1,18 @@
-// gulpfile.js
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 // compile only your main entry-point
 function buildStyles() {
   return src("./scss/style.scss")
     .pipe(sass().on("error", sass.logError))
+    .pipe(purgecss({ content: ["./html/*.html"] }))
     .pipe(dest("./css/"));
 }
 
 // watch but then call done()
 function watchTask(done) {
-  watch("./scss/**/*.scss", buildStyles);
+  watch(["./scss/**/*.scss", "./html/*.html"], buildStyles);
   done();
 }
 
